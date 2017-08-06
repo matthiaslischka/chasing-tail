f02444a5fdac68d6fd56facf92f23ea334f50327
Matthias Lischka <matthias.lischka@gmx.at>
2017-08-06 23:49:25 +0200

    basic generation of a snake body
    not quite yet what the goal is but a very basic start. must be changed to more block-like movements.

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,44 +1,65 @@
 var game = {};
 
 var cursors;
-var sprite;
+
+var snakeHead;
+var snakeSection = new Array();
+var snakePath = new Array();
+var numSnakeSections = 30;
+var snakeSpacer = 12;
 
 game.create = function () {
-  var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-  logo.anchor.setTo(0.5, 0.5);
-
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  sprite = this.game.add.sprite(200, 200, 'mushroom');
+  game.world.setBounds(0, 0, 800, 600);
 
   cursors = game.input.keyboard.createCursorKeys();
+
+  snakeHead = game.add.sprite(400, 300, 'mushroom');
+  snakeHead.anchor.setTo(0.5, 0.5);
+
+  game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
+
+  for (var i = 1; i <= numSnakeSections - 1; i++) {
+    snakeSection[i] = game.add.sprite(400, 300, 'mushroom');
+    snakeSection[i].anchor.setTo(0.5, 0.5);
+  }
+
+  for (var i = 0; i <= numSnakeSections * snakeSpacer; i++) {
+    snakePath[i] = new Phaser.Point(400, 300);
+  }
+
 };
 
-
-var speed = 4;
-var speedX = 0;
-var speedY = 0;
-
 game.update = function () {
-sprite2.body.setZeroVelocity();
-
-  if (cursors.left.isDown) {
-    speedX = -speed;
-  }
-  else if (cursors.right.isDown) {
-    speedX = speed;
-  }
+  snakeHead.body.velocity.setTo(0, 0);
+  snakeHead.body.angularVelocity = 0;
 
   if (cursors.up.isDown) {
-    speedY = -speed;
+    snakeHead.angle = -90;
   }
   else if (cursors.down.isDown) {
-    speedY = speed;
+    snakeHead.angle = 90;
+  }
+  else if (cursors.left.isDown) {
+    snakeHead.angle = -180;
+  }
+  else if (cursors.right.isDown) {
+    snakeHead.angle = 0;
+  }
+  snakeHead.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(snakeHead.angle, 300));
+
+  var part = snakePath.pop();
+
+  part.setTo(snakeHead.x, snakeHead.y);
+
+  snakePath.unshift(part);
+
+  for (var i = 1; i <= numSnakeSections - 1; i++) {
+    snakeSection[i].x = (snakePath[i * snakeSpacer]).x;
+    snakeSection[i].y = (snakePath[i * snakeSpacer]).y;
   }
 
-  sprite.body.moveLeft(400);
-  sprite.x += speedX;
-  sprite.y += speedY;
 };
 
 module.exports = game;
